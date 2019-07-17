bdb288af564765a7a6a202a8185c4f4152594464
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-07-17 15:41:04 +0100

    first button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-      count: 0
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
+
     return (
       <>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </>
     );
   }
-  formatCount(){
-      const { count } = this.state;
-        const x = <h1>Zero</h1>
-      return count === 0 ? x : count;
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+  formatCount() {
+    const { count } = this.state;
+    const x = <h1>Zero</h1>;
+    return count === 0 ? x : count;
   }
 }
-
 
 export default Counter;
