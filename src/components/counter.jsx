8c361c9363fa1d5a4cd80f46d020478eaf2080d2
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-07-17 17:43:10 +0100

    add list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
     imageUrl: "https://picsum.photos/200"
   };
+
+hendleIncrement = ()=> {
+    this.setState({count:this.state.count+1}); 
+}
+
   render() {
 
     return (
       <>
         <img src={this.state.imageUrl} alt="" />
+        <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button 
+        onClick={this.hendleIncrement}
+        className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+        </ul>
+        </div>
       </>
     );
   }
