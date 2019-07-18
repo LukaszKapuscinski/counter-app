99c1976bd20ff570e0b4ba31a031d814c1c95c5a
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-07-18 20:41:39 +0100

    add buttons delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
-    imageUrl: "https://picsum.photos/200"
+      count: 0,
+      value: this.props.value
+    
+
   };
 
 hendleIncrement = ()=> {
-    this.setState({count:this.state.count+1}); 
+    this.setState({count:this.state.value+1}); 
 }
 
   render() {
 
     return (
       <>
-        <img src={this.state.imageUrl} alt="" />
+      {this.props.children}
         <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button 
         onClick={this.hendleIncrement}
-        className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-            {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-        </ul>
+        className="btn btn-secondary btn-sm">Increment
+        </button>
+        <button onClick={this.handleDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+        
         </div>
       </>
     );
